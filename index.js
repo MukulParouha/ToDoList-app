d530c41163e9d139a5a30711a48cee160d56cdd7
Mukul Parouha <mukulparouha32@gmail.com>
2022-09-21 23:58:20 +0530

    second

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 var noOftasks=0;
 setTasksCount();
-function setTasksCount(){
+function setTasksCount()
+{
     var p=document.getElementById("total-count");
     p.innerHTML=noOftasks +" tasks left";
 }
 
 //delete button functionality:-to delete the tasks from tasks list
 deleteTask();
-function deleteTask(){
+function deleteTask()
+{
     let deleteList=document.getElementsByClassName("delete-tasks");
     var i;
-    for(i=0;i<deleteList.length;i++){
-        deleteList[i].addEventListener("click",function(){
+    for(i=0;i<deleteList.length;i++)
+    {
+        deleteList[i].addEventListener("click",function()
+        {
             var div=this.parentElement;
             noOftasks--;
             setTasksCount();
@@ -23,14 +27,16 @@ function deleteTask(){
 
 //ADD task button functionality:-To add the task in tasks list
 let addButton=document.getElementById("add-button");
-addButton.addEventListener("click",function(){
+addButton.addEventListener("click",function()
+{
     var inputValue=document.getElementById("task").value;
     var div=document.createElement("div");
     div.className="uncomplete";
     div.classList.add("tsk");
     var li=document.createElement("li");
     var i=document.createElement("i");
-    if(inputValue=== ""){
+    if(inputValue=== "")
+    {
         alert("You Must Write Something");
     }else{
         i.className="fas fa-trash delete-tasks";
@@ -49,22 +55,27 @@ addButton.addEventListener("click",function(){
 
 //All button functionality:to show both uncomplete and completed tasks
 var allButton=document.getElementById('all-button');
-allButton.addEventListener("click",function(){
+allButton.addEventListener("click",function()
+{
     var tasksList=document.getElementsByClassName("complete");
-    for(let i=0;i<tasksList.length;i++){
+    for(let i=0;i<tasksList.length;i++)
+    {
         tasksList[i].style.display='flex';
     }
     var tasksList=document.getElementsByClassName('uncomplete');
-    for(let i=0;i<tasksList.length;i++){
+    for(let i=0;i<tasksList.length;i++)
+    {
         tasksList[i].style.display='flex';
     }
 })
 
 //Completed button functionality:to show only completed tasks
 var completeButton=document.getElementById('complete-button');
-completeButton.addEventListener("click",function(){
+completeButton.addEventListener("click",function()
+{
     var tasksList=document.getElementsByClassName("complete");
-    for(let i=0;i<tasksList.length;i++){
+    for(let i=0;i<tasksList.length;i++)
+    {
         tasksList[i].style.display='flex';
         tasksList[i].classList.add("checked");
     }
@@ -76,9 +87,11 @@ completeButton.addEventListener("click",function(){
 
 //uncomplete button functionality:to show only uncomplete tasks
 var uncompleteButton=document.getElementById('uncomplete-button');
-uncompleteButton.addEventListener("click",function(){
+uncompleteButton.addEventListener("click",function()
+{
     var tasksList=document.getElementsByClassName("complete");
-    for(let i=0;i<tasksList.length;i++){
+    for(let i=0;i<tasksList.length;i++)
+    {
         tasksList[i].style.display='none';
     }
     var tasksList=document.getElementsByClassName('uncomplete');
@@ -91,10 +104,13 @@ uncompleteButton.addEventListener("click",function(){
 
 //complete all tasks button
 var completeAll=document.getElementById("complete-all");
-completeAll.addEventListener('click',function(){
+completeAll.addEventListener('click',function()
+{
     var totalDiv=document.querySelectorAll("ul .tsk");
-    for(var i=0;i<totalDiv.length;i++){
-       if(totalDiv[i].classList.contains("uncomplete")){
+    for(var i=0;i<totalDiv.length;i++)
+    {
+       if(totalDiv[i].classList.contains("uncomplete"))
+       {
            totalDiv[i].classList.remove("uncomplete");
            totalDiv[i].classList.add("complete"); 
        }
@@ -106,9 +122,11 @@ completeAll.addEventListener('click',function(){
 
 //Clear All Completed Task button
 var clearAllbtn=document.getElementById('clear-complete');
-clearAllbtn.addEventListener("click",function(){
+clearAllbtn.addEventListener("click",function()
+{
     var tasksList=document.querySelectorAll("ul .complete");
-    for(var i=0;i<tasksList.length;i++){
+    for(var i=0;i<tasksList.length;i++)
+    {
         tasksList[i].remove();
     }
  
@@ -117,8 +135,10 @@ clearAllbtn.addEventListener("click",function(){
 
 //toggling class complete and uncomplete when clicked on the list
 var list=document.querySelector('ul');
-list.addEventListener('click',function(e){
-    if(e.target.tagName==="DIV"){
+list.addEventListener('click',function(e)
+{
+    if(e.target.tagName==="DIV")
+    {
         e.target.classList.toggle("uncomplete")
         e.target.classList.toggle("complete");        
     }else if(e.target.tagName==="LI"){
